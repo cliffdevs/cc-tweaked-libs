9fd3299c74326c821c41049e914bb5fa42a2f6cd
Clifford Workman <cworkman@eborkdev.com>
2022-09-23 15:05:20 -0400

    Update update.lua
    - Add gitget installer

--- update.lua
+++ update.lua
@@ -1,2 +1,7 @@
 -- Execute this in the CLI to update your library to the latest version
+if !fs.exists("gitget") then
+    shell.run("pastebin get W5ZkVYSi gitget")
+end
+
 shell.run("gitget cliffdevs cc-tweaked-libs")
+
